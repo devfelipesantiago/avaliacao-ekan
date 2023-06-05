806ed94854af1e36acd434b4c76cc12dc198ffe4
Felipe Santiago <devfelipesantiago@gmail.com>
2023-06-05 13:43:04 -0300

    Init commit

---
+++ ekan/src/test/java/com/avaliacao/ekan/EkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avaliacao.ekan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
